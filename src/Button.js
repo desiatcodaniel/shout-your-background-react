5abe035ba44b662e416f8c8c1496e3cef9b69b06
desiatcodaniel <desiatcodaniel@gmail.com>
2021-04-29 20:19:46 -0700

    Delete unused files and style Button component

--- src/Button.js
+++ src/Button.js
@@ -1,11 +1,33 @@
-function Button(){
-    const buttonList = [
-        'original', 'grayscale', 'romantic', 'tropical'
-    ];
-    return(
-        buttonList.map(filter => <button key={filter} type="button" className="btn" id={filter}>{filter}</button>)
-    )
+function Button() {
+  const altStyles = [{
+    color: 'black',
+    background: 'white'
+},{
+    color: 'white',
+    background: 'black'
+}]
+  const buttonList = [
+      {
+        text : "Original",
+        style: altStyles[0],
+      },
+      {
+        text : "Grayscale",
+        style: altStyles[1],
+      },
+      {
+        text : "Romantic",
+        style: altStyles[0],
+      },
+      {
+        text : "Tropical",
+        style: altStyles[1],
+      }];
+  return buttonList.map((filter) => (
+    <button key={filter.text} style={filter.style} type="button" className="btn" id={filter.text}>
+      {filter.text}
+    </button>
+  ));
 }
 
-export default Button
-
+export default Button;
